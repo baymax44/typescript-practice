58eb5fad7502c35ea0f7792631aa19432cba73e7
baymax44 <smartbay2021@outlook.com>
2020-05-02 16:01:11 -0400

    dom & type casting

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
