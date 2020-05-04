1d03423e7d3fa5639379377d8eef3efcc99c3528
baymax44 <smartbay2021@outlook.com>
2020-05-04 16:18:42 -0400

    modules

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
+import { Invoice } from './classes/Invoice.js';
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+})
